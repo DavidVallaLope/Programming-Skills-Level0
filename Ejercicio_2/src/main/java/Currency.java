a18f10538eee64c480bb325bb54032e4b8b1d579
DavidVallaLope <davidvallalopez@gmail.com>
2024-01-24 19:08:40 -0600

    Add logic for converter

--- Ejercicio_2/src/main/java/Currency.java
+++ Ejercicio_2/src/main/java/Currency.java
@@ -1,14 +1,16 @@
 public enum Currency {
-    CLP(0.00025f),
-    ARS(0.0012f),
-    USD(1.0f),
-    EUR(1.09f),
-    TRY(0.033f),
-    GBP(1.27f);
+    CLP(0.00025f,"CLP"),
+    ARS(0.0012f,"ARS"),
+    USD(1.0f,"USD"),
+    EUR(1.09f,"EUR"),
+    TRY(0.033f,"TRY"),
+    GBP(1.27f,"GBP");
 
     final float value;
+    final String name;
 
-    private Currency(float value) {
+    Currency(float value, String name) {
         this.value = value;
+        this.name = name;
     }
 }
--- Ejercicio_2/src/main/java/Main.java
+++ Ejercicio_2/src/main/java/Main.java
@@ -5,10 +5,63 @@
 //* 		Set a minimum and maximum amount for each currency, it can be of your choice.
 //* 		The system should ask the user if they want to perform another operation. If they choose to do so, it should restart the process; otherwise, the system should close.
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String salida;
+        do {
+
+            System.out.println("Select initial currency");
+            printAvailableCurrency();
+            Integer initialCurrency = in.nextInt();
+
+            System.out.println("Select convert currency");
+            printAvailableCurrency();
+            Integer convertCurrency = in.nextInt();
+
+            System.out.println("Insert the quantity to convert");
+            Integer amount = in.nextInt();
+
+
+            float valueInitialCurrency = getAvailableCurrency(initialCurrency);
+            float valueConvertCurrency = getAvailableCurrency(convertCurrency);
+
+            System.out.println(convert(amount,valueInitialCurrency,valueConvertCurrency));
+
+            System.out.println("Desea salir?");
+            salida = in.nextLine();
+
+        } while (salida.equals("Y") || salida.equals("y"));
+    }
+
+    public static void printAvailableCurrency() {
+        StringBuilder curr = new StringBuilder();
+        curr.append("1. CLP \n");
+        curr.append("2. ARS \n");
+        curr.append("3. USD \n");
+        curr.append("4. EUR \n");
+        curr.append("5. TRY \n");
+        curr.append("6. GBP");
+        System.out.println(curr.toString());
+    }
+
+    public static float getAvailableCurrency(int option) {
+        return switch (option) {
+            case 1 -> Currency.CLP.value;
+            case 2 -> Currency.ARS.value;
+            case 3 -> Currency.USD.value;
+            case 4 -> Currency.EUR.value;
+            case 5 -> Currency.TRY.value;
+            case 6 -> Currency.GBP.value;
+            default -> 0f;
+        };
+    }
+
+    public static float convert (float valor, float cvalor1, float cvalor2) {
+        float constanteDeCambio = (1/cvalor2) / (1/cvalor1);
+        return valor * constanteDeCambio;
     }
 
 
